7466a2dce99bbea392a1d2e3c3c72b261a38a3a3
Tyler <tyler@briareos.net>
2014-09-02 10:37:52 -0400

    Cleanup

--- ThurdKind.ino
+++ ThurdKind.ino
@@ -398,27 +398,6 @@ long hsv2rgb(long h, byte s, byte v) {
   // 1 to allow shifts, and upgrade to long makes other conversions implicit.
   v1 = v + 1;
   return (((r * v1) & 0xff00) << 8) |
-    ((g * v1) & 0xff00)       |
-    ( (b * v1)           >> 8);
+    ((g * v1) & 0xff00) |
+    ( (b * v1) >> 8);
 }
-
-// The fixed-point sine and cosine functions use marginally more
-// conventional units, equal to 1/2 degree (720 units around full circle),
-// chosen because this gives a reasonable resolution for the given output
-// range (-127 to +127).  Sine table intentionally contains 181 (not 180)
-// elements: 0 to 180 *inclusive*.  This is normal.
-
-PROGMEM prog_char sineTable[181]  = {
-  0,  1,  2,  3,  5,  6,  7,  8,  9, 10, 11, 12, 13, 15, 16, 17,
-  18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 29, 30, 31, 32, 33, 34,
-  35, 36, 37, 38, 39, 40, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
-  52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
-  67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 77, 78, 79, 80, 81,
-  82, 83, 83, 84, 85, 86, 87, 88, 88, 89, 90, 91, 92, 92, 93, 94,
-  95, 95, 96, 97, 97, 98, 99,100,100,101,102,102,103,104,104,105,
-  105,106,107,107,108,108,109,110,110,111,111,112,112,113,113,114,
-  114,115,115,116,116,117,117,117,118,118,119,119,120,120,120,121,
-  121,121,122,122,122,123,123,123,123,124,124,124,124,125,125,125,
-  125,125,126,126,126,126,126,126,126,127,127,127,127,127,127,127,
-  127,127,127,127,127
-};
